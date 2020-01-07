d925b49e1dc65718385cf2beb7091525d47e26b3
at-vuongtran <vuong.tran@asiantech.vn>
2020-01-07 17:59:35 +0700

    fix condition if else block scope

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ var anwser5 = document.getElementById('question5');
 
 anwser1.append('typeof null = object, typeof undefined = undefined. bien khai bao nhung ko dinh nghia gia tri thi mac dinh bang undefine, null duoc khai bao vi du "var a = null". ');
 anwser2.append('user-trict la che do kiem tra code nghiem ngat duoc dat o dau khoi code. Khong the su dung cac bien, function ma khong khai bao truoc... Viec su dung use-trict giup code chat che, de hieu hon, chinh xac hon.')
-anwser3.append('== so sanh tuong doi, === so sanh tuyet doi kiem tra kieu du lieu co giong nhau hay khonng. khi so sanh == du lieu bi ep kieu roi so sanh. Vi du xem tai phan console')
+anwser3.append('== so sanh tuong doi, === so sanh tuyet doi kiem tra them kieu du lieu co giong nhau hay khonng. khi so sanh == du lieu bi ep kieu roi so sanh. Vi du xem tai phan console')
 
-function show_difference(){
+function show_difference() {
   var a = '';
-  if (a == 0) console.log('so sanh == ket qua: true');
-  if (a === 0) console.log('so sanh === ket qua: true');
-  else console.log('so sanh === ket qua: false');
+  if (a == 0) { console.log('so sanh == ket qua: true'); }
+  //difference with
+  if (a === 0) { 
+    console.log('so sanh === ket qua: true');
+  } else { 
+    console.log('so sanh === ket qua: false');
+  }
 }
 console.log(show_difference);
 show_difference();
@@ -21,26 +25,28 @@ anwser4.append('Voi var bien co kha nang truy cap o moi cho ngoai ra ta co the v
 anwser5.append('there are undefine, null, NAN, "", 0, false.');
 
 console.log('===============================================================');
-function sum(a, b){
-  return a !== b ? a + b : (a +b) * 3;
+var arr0 = ['0', '3', '6', '9'];
+var arr1 = ['2', '5', '8'];
+var arr2 = ['1', '4', '7'];
+
+function sum(a, b) {
+  return (a + b) * (a === b) ? 3 : 1;
 }
+
 console.log('1.');
 console.log(sum);
 console.log(sum(5, 10), sum(5, 5))
 
-function absoluteDifference(a){
-  return 19 - a >= 0 ? 19 - a : 3 * (a - 19)
+function absoluteDifference(a) {
+  return (19 - a) * ((19 - a) >= 0) ? 1 : -3;
 }
+
 console.log('2.');
 console.log(absoluteDifference);
 console.log(absoluteDifference(12), absoluteDifference(22));
 
-var arr0 = ['0', '3', '6', '9'];
-var arr1 = ['2', '5', '8'];
-var arr2 = ['1', '4', '7'];
-var result;
-
-function findNumberDivided(str){
+function findNumberDivided(str) {
+  var result;
   var sum = 0;
   for (var i = 0; i < str.length; i++) {
     if (str.charAt(i) !== '*') {
@@ -48,13 +54,11 @@ function findNumberDivided(str){
     }
   }
   if (sum % 3 === 0) {
-    result = arr0.map((item)=>str.replace('*', item));
-  }
-  else if (sum % 3 === 1) {
-    result = arr1.map((item)=>str.replace('*', item));
-  }
-  else {
-    result = arr2.map((item)=>str.replace('*', item));
+    result = arr0.map((item) => str.replace('*', item));
+  } else if (sum % 3 === 1) {
+    result = arr1.map((item) => str.replace('*', item));
+  } else {
+    result = arr2.map((item) => str.replace('*', item));
   }
   return result;
 }
@@ -63,11 +67,9 @@ console.log('3.');
 console.log(findNumberDivided);
 console.log(findNumberDivided('1234567890*'), findNumberDivided('1*9'));
 
-function findNumberDivided6(str){
+function findNumberDivided6(str) {
   var divided3 = findNumberDivided(str);
-  var result = divided3.filter(function(item) {
-    return Number(item.charAt(item.length - 1)) % 2 === 0;
-  });
+  var result = divided3.filter((item) => Number(item.charAt(item.length - 1)) % 2 === 0);
   return result;
 }
 
